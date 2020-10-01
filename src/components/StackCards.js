ce4ee88807be9434da4b3de945d33fb7486a3aef
Tim <tim.lisiecki@gmail.com>
2020-10-01 08:31:47 -0400

    update relative links in components dynamically adding static scripts

--- src/components/StackCards.js
+++ src/components/StackCards.js
@@ -5,7 +5,7 @@ import Banner from './Banner';
 function StackCards() {
 	useEffect(() => {
 		let frontEnd = document.createElement('script');
-		frontEnd.src = '/blue-collar-coding/static/js/_1_stacking-cards.js';
+		frontEnd.src = '/static/js/_1_stacking-cards.js';
 		frontEnd.id = 'stacking-card-js';
 		document.body.appendChild(frontEnd);
 	}, []);
